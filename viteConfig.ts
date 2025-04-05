f32970f24ae8cdf55025ca6ce04b9fa3693550d6
Victor Xavier Rodriguez <victorxr1994@gmail.com>
2025-04-05 14:17:20 +0200

    Solucionado error en el archivo viteConfig.ts

--- viteConfig.ts
+++ viteConfig.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/psychowell/', // 👈 muy importante para GitHub Pages
+  base: '/Psychowell/', // 👈 muy importante para GitHub Pages
   plugins: [react()]
 })
